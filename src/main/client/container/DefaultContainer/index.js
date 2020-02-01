0a51cff9d4d38b8fd15862d0b2f4ede36fb2b3a5
Shivendra Verma <shivendra.verma@navyuginfo.com>
2020-02-01 14:35:46 +0530

    add loaders, add git egnore webapp, yarn.lock and build concept

--- src/main/client/container/DefaultContainer/index.js
+++ src/main/client/container/DefaultContainer/index.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import RoutesToRender from './routes';
+import './style.scss';
 // This class contains fixed components to render including the switch routing which is dynamic and is managed by routes
 
 
