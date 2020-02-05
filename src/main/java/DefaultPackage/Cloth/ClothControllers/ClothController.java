79fb53587005ba7405122c712e7f432b063c3d3d
Shivendra Verma <Shivendrasony@gmail.com>
2020-02-05 16:00:03 +0530

    add POST API for cloth

--- src/main/java/DefaultPackage/Cloth/ClothControllers/ClothController.java
+++ src/main/java/DefaultPackage/Cloth/ClothControllers/ClothController.java
@@ -1,6 +1,9 @@
 package DefaultPackage.Cloth.ClothControllers;
 
 
+import DefaultPackage.Cloth.ClothModels.ClothModal;
+import DefaultPackage.Cloth.ClothRepositories.ClothRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -9,8 +12,18 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/app")
 public class ClothController {
+
+    @Autowired
+    private ClothRepository clothRepositor;
+
     @RequestMapping(value = "/rest/v1/cloth", method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity addCloth(){
         return new ResponseEntity<>("Hello World!", HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/rest/v1/add-cloth", method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity createCloth(@RequestBody ClothModal clothModal) throws Exception {
+        clothModal = clothRepositor.save(clothModal);
+        return new ResponseEntity<>("Success",HttpStatus.OK);
     }
 }
--- src/main/java/DefaultPackage/Cloth/ClothRepositories/ClothRepository.java
+++ src/main/java/DefaultPackage/Cloth/ClothRepositories/ClothRepository.java
@@ -2,10 +2,11 @@ package DefaultPackage.Cloth.ClothRepositories;
 
 import DefaultPackage.Cloth.ClothModels.ClothModal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public  interface  ClothRepository extends JpaRepository<ClothModal, Long> {
 
-    
 }
